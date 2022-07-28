df9eb36a8119832ca036477f82893478b6068391
Yeshwanth <36299877+yeshwanthc@users.noreply.github.com>
2022-07-28 11:05:32 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes , Route } from "react-router-dom";
+import Navigation from "./routes/navBar/navBar";
+import Home from "./routes/home/home.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+const Shop =()=>{
+  return <h1>Iam shop page</h1>
+};
+
+const App = () => {
+  return(
+      <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={ <Home />} />
+        <Route path='shop' element={ <Shop />} />
+        <Route path='sign-in' element={ <SignIn />} />
+        </Route>
+      </Routes>
   );
-}
-
+};
 export default App;
