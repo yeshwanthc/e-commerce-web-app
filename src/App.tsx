2caf43ed4c6e2837a1cfe6487568a7b9d82996af
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-10-19 22:14:38 +0530

    added seperate home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import HeroSlider from "./components/Hero/Hero";
-import FeaturedProducts from "./components/Products/FeaturedProducts";
+import Home from "./Home";
+
 
 function App() {
   return (
     <>
-      <Header />
-      <HeroSlider />
-      <FeaturedProducts />
-      <Footer />
+    <Home />
     </>
   );
 }
