a040843435b0b038404570d15526b795d55167bb
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-11-01 23:17:38 +0530

    custom-hook

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
